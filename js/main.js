6fca460bcb11103f952fcc0a64126c33fe4ac631
Julian David Perez <julianjp@outlook.com>
2018-10-12 13:03:05 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+;
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+}); 
+document.addEventListener("DOMContentLoaded", function(){
+	$('.preloader-background').delay(1700).fadeOut('slow');
+	
+	$('.preloader-wrapper')
+		.delay(1700)
+		.fadeOut();
+});
